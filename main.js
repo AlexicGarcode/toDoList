c24da8bde16c7de9e579d1d70f5afbadf89d0ea0
AlexAutomation <80054118+AlexicGarcode@users.noreply.github.com>
2022-10-17 15:13:20 -0500

    first commit

---
+++ main.js
@@ -0,0 +1,12 @@
+const taskInput = document.getElementById("new-task");
+const addButton = document.getElementsByTagName("button")[0];
+const incompleteTasksHolder = document.getElementById("imcomplete-tasks");
+const completedTaskHolder = document.getElementById("completed-tasks");
+
+
+//New Task List Item
+
+const createNewTaskElement = function(taskString){
+    const listenItem = document.createElement("li");
+    
+}
